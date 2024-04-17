76ed932cb617e11d352b06caac9bbf087cbe061b
eshref04 <esrefsefiyev@gmail.com>
2024-04-17 19:45:35 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// let left=document.querySelector(".left");
+// let  icon=document.querySelector(".fa-x");
+// let  button=document.querySelector(".btn");
+
+// button.addEventListener("click",()=>{
+//     left.style.transform="translateX(0)"
+// })
+
+
+
+
+
+
+// let oneprgrph=document.querySelectorAll(".oneprgrph");
+// for (let index = 0; index < oneprgrph.length; index++) {
+//    oneprgrph[index].addEventListener("click",function(){
+//     this.classList.toggle("active")
+//    })
+    
+// }
